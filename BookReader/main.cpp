0303df7b0b6f32f4d1cbec4c8c9e2e717b49c5fe
mohmed-hussein <mohamd.h.yousuf@gmail.com>
2024-02-05 22:56:16 +0200

    complete user and admin class

--- BookReader/main.cpp
+++ BookReader/main.cpp
@@ -2,7 +2,16 @@
 #include <vector>
 #include <ctime>
 using namespace std;
-
+class HelperFunction{
+public:
+    static int getNumberInRange(int low , int high){
+        cout<<"Enter number in Range ["<<low<<','<<high<<"] : ";
+        int numbere ;cin>>numbere;
+        if(numbere < low || numbere > high)
+            return getNumberInRange(low , high);
+        return numbere;
+    }
+};
 class Book{
 private:
     int numberPages , curPage;
@@ -31,9 +40,16 @@ public:
     bool hasNext() const {
         return(curPage < numberPages);
     }
+    bool hasPrevious() const{
+        return (getCurPageNumber() > 0);
+    }
     const string &getNext() {
-        if(!this->hasNext()) return "";
+        if(!this->hasNext()) return pages[curPage - 1];
         return pages[curPage++];
+    }
+    const string & getPrevious(){
+        if(!this->hasPrevious()) return pages[curPage + 1];
+        return pages[--curPage];
     }
     int getCurPageNumber() const{
         return curPage+1;
@@ -55,20 +71,36 @@ public:
         }
     }
     void printInfo() const {
-        cout<<"Book Name : " << getBookTitle() <<endl;
-        cout<<"Number of Pages : " << getNumberPages() << endl;
+        cout<< getBookTitle() <<endl;
     }
 };
 
 class library{
 private:
     vector<Book> books;
+    library(){};
+     static library * lib;
 public:
-    void addBook(const Book & newBook){
+    static library * getLibrary(){
+         if(lib == nullptr){
+             lib = new library;
+         }
+        return lib;
+    }
+    void addBook(){
+        Book newBook;
+        newBook.readNewBook();
         books.emplace_back(newBook);
     }
     void listBooks() const {
-        for(auto book : books) book.printInfo();
+        int idx = 1;
+        for(auto book : books){
+            cout<<idx << ": ";
+            book.printInfo();
+        }
+    }
+    int getNumberOfBooks(){
+        return books.size();
     }
     Book selectBook(int idx) const {
         return books[idx - 1];
@@ -81,13 +113,117 @@ private:
     time_t sessionDate;
     int userID;
 public:
-    session(const Book &book, time_t Date, int userId) :
-        book(book), sessionDate(Date), userID(userId) {}
+    session(){}
+    session(const Book & _book , int ID) :
+        book(_book) , userID(ID) , sessionDate(time(nullptr)){}
+    void update(const Book & _book){
+        book = _book;
+        sessionDate = time(nullptr);
+    }
+
+     Book getBook() const {
+        return book;
+    }
 
     void printSessionInfo() const {
-        cout<<"Book Name : " << book.getBookTitle()<<endl;
-        cout<<"Current Page " << book.getCurPageNumber()<<endl;
-        cout<<sessionDate<<endl;
+        cout<<book.getBookTitle()<<' ';
+        cout<<'('<<book.getCurPageNumber() << '/' << book.getNumberPages() <<')';
+        cout<<' ' << ctime(&sessionDate)<<endl;
+    }
+};
+class person{
+private:
+    string name , userName , passWord , email;
+    int Id;
+    bool isAdmin;
+public:
+    person() : person("" , "" , "" , "" , 0 ){}
+    person(const string &name, const string &userName, const string &passWord, const string &email, bool isAdmin)
+            : name(name), userName(userName), passWord(passWord), email(email), isAdmin(isAdmin) {}
+
+public:
+    void viewProfile() const{
+        cout<<"Name : " << this->name<<endl;
+        cout<<"UserName : " << this->userName<<endl;
+    }
+
+    const string &getUserName() const {
+        return userName;
+    }
+
+    const string &getPassWord() const {
+        return passWord;
+    }
+
+    int getId() const {
+        return Id;
+    }
+
+    void setId(int id) {
+        Id = id;
+    }
+
+    bool isAdmin1() const {
+        return isAdmin;
+    }
+
+};
+
+class user : public person{
+private:
+    vector<session> sessions;
+public:
+    user() : person(){};
+    user(const string &name, const string &userName, const string &passWord, const string &email, bool isAdmin)
+            : person(name, userName, passWord, email, isAdmin) {}
+    void readBook(Book & book){
+        int choice = 1;
+        while(true){
+            cout<<"Current Page : " << book.getCurPageNumber() <<'/' << book.getNumberPages()<<endl;
+            if(choice == 1)
+                cout<<book.getNext()<<endl;
+            else if(choice == 2)
+                cout<<book.getPrevious()<<endl;
+            else break;
+
+            choice = HelperFunction::getNumberInRange(1 , 3);
+            cout<<"Menu :" << endl;
+            cout<<'\t' << "1: Next Page"<<endl;
+            cout<<'\t' << "2: Previous Page"<<endl;
+            cout<<'\t' << "3: Stop Reading"<<endl;
+        }
+    }
+
+    void listSelectFromHistory(){
+        cout<<"Your Seessions is : ";
+        for(auto it : sessions)
+                it.printSessionInfo();
+        cout<<"Which session to open : ";
+        int sessionIdx = HelperFunction::getNumberInRange(1 , sessions.size());
+        Book book = sessions[sessionIdx].getBook();
+        readBook(book);
+        sessions[sessionIdx].update(book);
+    }
+
+    void listSelectFromLibrary(){
+        cout<<"Our Current Books Collection : "<<endl;
+        library * lib = library::getLibrary();
+        lib->listBooks();
+        int bookIdx = HelperFunction::getNumberInRange(1 , lib->getNumberOfBooks());
+        Book _book = lib->selectBook(bookIdx);
+        readBook(_book);
+        sessions.emplace_back(session(_book , this->getId()));
+    }
+};
+
+class admin : public person{
+public:
+    admin(const string &name, const string &userName, const string &passWord, const string &email, bool isAdmin = 1)
+            : person(name, userName, passWord, email, isAdmin) {}
+
+    void AddBook(){
+        library * lib = library::getLibrary();
+        lib->addBook();
     }
 };
 int main() {
